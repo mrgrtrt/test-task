c551ae534267c8f3cc0202fa086ffaf900c913fa
mrgrtrt <mrgrtrt@users.noreply.github.com>
2022-11-24 20:11:40 +0000

    deploy: a234fa45d959d9f9d6ae240c110f6797db0f4d0f

--- src/main/kotlin/Todo.kt
+++
@@ -1,7 +0,0 @@
-import kotlin.random.Random
-
-data class Todo(
-    val id: Long = Random.nextLong(0, 99999),
-    val text: String = "Todo${Random.nextInt(1, 100)}",
-    val completed: Boolean = true
-)
